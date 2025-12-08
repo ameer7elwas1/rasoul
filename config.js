1c7f9ff5e89c7c6dbf10cc1a542d31003408e123
ameer7elwas1 <ameermohammed190@gmail.com>
2025-12-08 17:02:37 +0300

    إضافة مفتاح Supabase ANON_KEY

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     SUPABASE: {
         URL: 'https://vpvvjascwgivdjyyhzwp.supabase.co',
-        ANON_KEY: ''
+        ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZwdnZqYXNjd2dpdmRqeXloendwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk4MDYxMjYsImV4cCI6MjA2NTM4MjEyNn0.6AR2-MG4x9ugNTXe9jUqx-IwGEtj1m6MCYwQkTsSbUQ'
     },
     SECURITY: {
         MAX_LOGIN_ATTEMPTS: 5,
